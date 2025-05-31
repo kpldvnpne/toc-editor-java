082615c0834715a29be84827f74cd14af03bf444
Kapildev Neupane <kapildevneupane55@gmail.com>
2025-05-31 17:18:47 -0400

    Add multiple  TOC

--- src/Main.java
+++ src/Main.java
@@ -16,15 +16,25 @@ public class Main {
         var root = document.getOutlines(true);
 
         // Remove every child
+        // TODO: Removing every outline does not work
         while (root.getAllChildren().size() > 1) {
           root.getAllChildren().get(0).removeOutline();
         }
+
+        // NOTE: MacOS Preview does not show TOC unless there are two of them
         var childOutline = root.addOutline("Go to page 12", 1);
         var page = document.getPage(12);
         var destination = PdfExplicitDestination.createFit(page);
         childOutline.addDestination(destination);
+
+        childOutline = root.addOutline("Go to page 50", 2);
+        page = document.getPage(50);
+        destination = PdfExplicitDestination.createFit(page);
+        childOutline.addDestination(destination);
+
         System.out.println("Added some destination to outline");
 
+        // TODO: Remove this
         // Remove the first element, that we didn't remove because it does not work without it
         root.getAllChildren().get(0).removeOutline();
       }
