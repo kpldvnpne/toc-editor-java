9bb1fc05fded6bdfb3c23be327ba176b1378940f
Kapildev Neupane <kapildevneupane55@gmail.com>
2025-05-31 18:04:22 -0400

    Make the whole TOC work

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ import com.itextpdf.kernel.pdf.PdfWriter;
 import com.itextpdf.kernel.pdf.navigation.PdfExplicitDestination;
 
 public class Main {
+
   public static void main(String[] args) {
     String inputFilename = "/Users/kapildev/Downloads/ielts 15 academic/Cambridge 15 - Full Version.pdf";
     String outputFilename = "/Users/kapildev/Downloads/ielts 15 academic/Cambridge 15 - Full Version - JAVA TOC.pdf";
@@ -21,18 +22,7 @@ public class Main {
           root.getAllChildren().get(0).removeOutline();
         }
 
-        // NOTE: MacOS Preview does not show TOC unless there are two of them
-        var childOutline = root.addOutline("Go to page 12", 1);
-        var page = document.getPage(12);
-        var destination = PdfExplicitDestination.createFit(page);
-        childOutline.addDestination(destination);
-
-        childOutline = root.addOutline("Go to page 50", 2);
-        page = document.getPage(50);
-        destination = PdfExplicitDestination.createFit(page);
-        childOutline.addDestination(destination);
-
-        System.out.println("Added some destination to outline");
+        TocItem.outline.addChildrenTo(root, document);
 
         // TODO: Remove this
         // Remove the first element, that we didn't remove because it does not work without it
