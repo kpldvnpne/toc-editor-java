936a7c1e19f97ecfd064400f325544a97a4d2cd4
Kapildev Neupane <kapildevneupane55@gmail.com>
2025-05-31 16:28:18 -0400

    Make it work with Document and Reader

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,15 @@
-import tryjar.Practice;
+
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfReader;
 
 public class Main {
   public static void main(String[] args) {
-    // String filename = "/Users/kapildev/Downloads/ielts 15 academic/Cambridge 15 - Full Version.pdf";
-    // PdfReader reader = PdfReader(filename);
-    // PdfDocument document = PdfDocument(reader);
-    Practice.printHello();
+    String filename = "/Users/kapildev/Downloads/ielts 15 academic/Cambridge 15 - Full Version.pdf";
+    try {
+      PdfReader reader = new PdfReader(filename);
+      PdfDocument document = new PdfDocument(reader);
+    } catch (Exception exception) {
+      System.out.println("Could not open the file");
+    }
   }
 }
