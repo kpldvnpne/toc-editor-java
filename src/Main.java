a0e1de0f32cbd053c3aaf5bdbd3eb128592a81bb
Kapildev Neupane <kapildevneupane55@gmail.com>
2025-05-31 17:11:21 -0400

    Add TOC for page 12

--- src/Main.java
+++ src/Main.java
@@ -16,20 +16,14 @@ public class Main {
         var root = document.getOutlines(true);
 
         // Remove every child
-        while (!root.getAllChildren().isEmpty()) {
+        while (root.getAllChildren().size() > 1) {
           root.getAllChildren().get(0).removeOutline();
         }
-
-        // root.removeOutline();
-        // document.initializeOutlines();
-        // for (var child: root.getAllChildren()) {
-        //   child.removeOutline();
-        // }
-        // var childOutline = root.addOutline("Go to page 12");
-        // var page = document.getPage(12);
-        // var destination = PdfExplicitDestination.createFit(page);
-        // childOutline.addDestination(destination);
-        // System.out.println("Added some destination to outline");
+        var childOutline = root.addOutline("Go to page 12");
+        var page = document.getPage(12);
+        var destination = PdfExplicitDestination.createFit(page);
+        childOutline.addDestination(destination);
+        System.out.println("Added some destination to outline");
       }
     } catch (Exception exception) {
       System.out.println("Could not open the file");
