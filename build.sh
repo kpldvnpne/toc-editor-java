442f8647861ee5c98d39fddb157e1e5361eb5b87
Kapildev Neupane <kapildevneupane55@gmail.com>
2025-05-31 14:57:30 -0400

    Initial commit

---
+++ build.sh
@@ -0,0 +1,1 @@
+javac -cp "./lib/itext/kernel-9.2.0.jar" ./src/Main.java -d ./bin
---
+++ run.sh
@@ -0,0 +1,5 @@
+# Alternative: java  -cp "./iText-Core-9.2.0-only-jars/kernel-9.2.0.jar" Main.java
+
+./build.sh
+cd bin
+java Main
