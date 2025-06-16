3cce9836465183c5407621779a015f052da3703f
Kapildev Neupane <kapildevneupane55@gmail.com>
2025-06-16 09:20:46 -0400

    Fix run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,23 @@
 # Alternative: java  -cp "./iText-Core-9.2.0-only-jars/kernel-9.2.0.jar" Main.java
 
-./build.sh
-cd bin
-java -cp "../lib/try.jar:." Main
+OUT_DIR=bin
+
+# Remove existing output
+rm -rf $OUT_DIR/*
+
+# Class paths --- START
+slf4j='lib/slf4j-api-2.0.17.jar'
+
+# iText only
+kernel='lib/itext/kernel-9.2.0.jar'
+io='lib/itext/io-9.2.0.jar'
+layout='lib/itext/layout-9.2.0'
+bouncy_castle_adapter='lib/itext/bouncy-castle-adapter-9.2.0'
+commons='lib/itext/commons-9.2.0.jar'
+
+itext_cp="$kernel:$io:$layout:$bouncy_castle_adapter:$commons"
+whole_cp="$itext_cp:$slf4j"
+# Class paths --- END
+
+javac -sourcepath src -classpath $whole_cp -d $OUT_DIR src/Main.java
+java -classpath "$OUT_DIR:$whole_cp" Main
